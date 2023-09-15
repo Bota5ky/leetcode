7ba7f56c757de3105b6ca97bc9510971e4e3d251
Bota5ky <zjy2516@126.com>
2023-09-15 15:10:03 +0800

    pass daily problem 630 - course schedule iii

---
+++ common/question_451_to_750/course_schedule_iii/Solution.java
@@ -0,0 +1,35 @@
+package common.question_451_to_750.course_schedule_iii;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/course-schedule-iii/">630. 课程表 III</a>
+ * @since 2023-09-15 14:54
+ */
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, Comparator.comparingInt(a -> a[1]));
+        int total = 0;
+
+        var priorQueue = new PriorityQueue<Integer>((a, b) -> (b - a));
+
+        for (int[] course : courses) {
+            var costTime = course[0];
+            var endDate = course[1];
+            if (costTime + total <= endDate) {
+                total += costTime;
+                priorQueue.offer(costTime);
+            } else {
+                if (!priorQueue.isEmpty() && costTime < priorQueue.peek()) {
+                    total -= priorQueue.poll() - costTime;
+                    priorQueue.offer(costTime);
+                }
+            }
+        }
+
+        return priorQueue.size();
+    }
+}
