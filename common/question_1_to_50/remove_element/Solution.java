2289b6d6226b32d0fd381446d8b99e3a0fc0e509
Bota5ky <zjy2516@126.com>
2023-09-15 15:50:54 +0800

    pass study plan top interview 150 - remove element

---
+++ common/question_1_to_50/remove_element/Solution.java
@@ -0,0 +1,21 @@
+package common.question_1_to_50.remove_element;
+
+import java.util.Arrays;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/remove-element/">27. 移除元素</a>
+ * @since 2023-09-15 15:42
+ */
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        var last = nums.length - 1;
+        for (int i = 0; i < nums.length && i <= last; i++) {
+            if (nums[i] == val) {
+                nums[i--] = nums[last--];
+            }
+        }
+        nums = Arrays.copyOf(nums, last + 1);
+        return nums.length;
+    }
+}
