ad759e045fc5e5993d27b6c793eb5189dc11e929
Bota5ky <zjy2516@126.com>
2023-09-15 11:13:42 +0800

    pass daily problem LCP 50 - diamond supply

--- leetcodecup/lcp17/Solution.java
+++ leetcodecup/lcp17/Solution.java
@@ -1,11 +1,11 @@
-package common;
+package leetcodecup.lcp17;
 
 /**
  * @author Bota5ky
  * @link <a href="https://leetcode.cn/problems/nGK0Fy/">LCP 17. 速算机器人</a>
  * @since 2023-09-06 23:23
  */
-class LCP17 {
+class Solution {
     public int calculate(String s) {
         int x = 1, y = 0;
         for (int i = 0; i < s.length(); i++) {
---
+++ leetcodecup/lcp50/Solution.java
@@ -0,0 +1,19 @@
+package leetcodecup.lcp50;
+
+import java.util.Arrays;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/WHnhjV/">LCP 50. 宝石补给</a>
+ * @since 2023-09-15 11:06
+ */
+class Solution {
+    public int giveGem(int[] gem, int[][] operations) {
+        for (int[] operation : operations) {
+            gem[operation[1]] += gem[operation[0]] / 2;
+            gem[operation[0]] -= gem[operation[0]] / 2;
+        }
+        Arrays.sort(gem);
+        return gem[gem.length - 1] - gem[0];
+    }
+}
