7dfa23c7faaa2ca0ee5aaba35981bebb406103a5
Bota5ky <zjy2516@126.com>
2023-09-16 10:03:13 +0800

    pass daily problem 198 - house robber

---
+++ common/question_151_to_200/house_robber/Solution.java
@@ -0,0 +1,16 @@
+package common.question_151_to_200.house_robber;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/house-robber/">198. 打家劫舍</a>
+ * @since 2023-09-16 09:57
+ */
+class Solution {
+    public int rob(int[] nums) {
+        var dp = new int[nums.length + 2];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i + 2] = Math.max(dp[i] + nums[i], dp[i + 1]);
+        }
+        return dp[dp.length - 1];
+    }
+}
--- common/question_151_to_200/house_robber/solution.go
+++ common/question_151_to_200/house_robber/solution.go
@@ -11,14 +11,14 @@ func rob(nums []int) int {
 	//dp[i] = max(dp[i-2]+nums[i], dp[i-1])
 	dp := make([]int, len(nums))
 	dp[0] = nums[0]
-	dp[1] = max(nums[0], nums[1])
+	dp[1] = maxInt(nums[0], nums[1])
 	for i := 2; i < len(nums); i++ {
-		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+		dp[i] = maxInt(dp[i-2]+nums[i], dp[i-1])
 	}
 	return dp[len(nums)-1]
 }
 
-func max(a, b int) int {
+func maxInt(a, b int) int {
 	if a > b {
 		return a
 	}
