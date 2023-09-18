d9eab783ae30ccbc8210966ad9710f1aa14972bb
Bota5ky <zjy2516@126.com>
2023-09-18 13:51:07 +0800

    pass study plan top interview 150 - h index

---
+++ common/question_201_to_300/h_Index/Solution.java
@@ -0,0 +1,19 @@
+package common.question_201_to_300.h_Index;
+
+import java.util.Arrays;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/h-index/">274. H 指数</a>
+ * @since 2023-09-18 13:09
+ */
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        var h = 0;
+        for (int i = citations.length - 1; i >= 0 && citations[i] > h; i--) {
+            h++;
+        }
+        return h;
+    }
+}
