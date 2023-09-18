b8b86a1474fafb2972f869514e60b24fdee118aa
Bota5ky <zjy2516@126.com>
2023-09-18 10:43:11 +0800

    pass daily problem 337 - house robber iii

---
+++ common/question_301_to_450/house_robber_iii/Solution.java
@@ -0,0 +1,26 @@
+package common.question_301_to_450.house_robber_iii;
+
+import model.TreeNode;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/house-robber-iii/">337. 打家劫舍 III</a>
+ * @since 2023-09-18 09:51
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        var maxRob = dfs(root);
+        return Math.max(maxRob[0], maxRob[1]);
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+        int[] r = dfs(root.right);
+        int[] l = dfs(root.left);
+        var robCurrent = root.val + r[1] + l[1];
+        var notRobCurrent = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{robCurrent, notRobCurrent};
+    }
+}
