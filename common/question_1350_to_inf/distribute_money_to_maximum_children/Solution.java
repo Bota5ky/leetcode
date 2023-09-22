c579f192a51e5be091d5322c191fff61888739dc
Bota5ky <zjy2516@126.com>
2023-09-22 11:15:18 +0800

    pass study plan top interview 150 - distribute money to maximum children

---
+++ common/question_1350_to_inf/distribute_money_to_maximum_children/Solution.java
@@ -0,0 +1,21 @@
+package common.question_1350_to_inf.distribute_money_to_maximum_children;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/distribute-money-to-maximum-children/">2591. 将钱分给最多的儿童</a>
+ * @since 2023-09-22 10:24
+ */
+class Solution {
+    public int distMoney(int money, int children) {
+        money -= children;
+        if (money < 0) {
+            return -1;
+        }
+        var max = Math.min(money / 7, children);
+        var remain = money - 7 * max;
+        if (remain == 3 && max == children - 1 || max == children && remain > 0) {
+            return max - 1;
+        }
+        return max;
+    }
+}
