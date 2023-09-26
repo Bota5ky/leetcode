71b67c06bb44fd44d1d3ab09771bab35734664a8
Bota5ky <zjy2516@126.com>
2023-09-26 10:13:15 +0800

    pass daily problem 2582 - pass the pillow

---
+++ common/question_1350_to_inf/pass_the_pillow/Solution.java
@@ -0,0 +1,17 @@
+package common.question_1350_to_inf.pass_the_pillow;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/pass-the-pillow/">2582. 递枕头</a>
+ * @since 2023-09-26 09:59
+ */
+class Solution {
+    public int passThePillow(int n, int time) {
+        var round = time / (n - 1);
+        var remain = time % (n - 1);
+        if (round % 2 == 1) {
+            return n - remain;
+        }
+        return 1 + remain;
+    }
+}
