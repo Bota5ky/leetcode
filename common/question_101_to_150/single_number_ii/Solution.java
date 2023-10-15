55c6df3750085b27fba80012f5b55c6ecbc47c5d
Bota5ky <zjy2516@126.com>
2023-10-15 09:22:20 +0800

    pass daily problem 137 - single number ii

---
+++ common/question_101_to_150/single_number_ii/Solution.java
@@ -0,0 +1,18 @@
+package common.question_101_to_150.single_number_ii;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/single-number-ii/">137. 只出现一次的数字 II</a>
+ * @since 2023-10-15 09:21
+ */
+class Solution {
+    public int singleNumber(int[] nums) {
+        var ones = 0;
+        var twos = 0;
+        for (int num : nums) {
+            ones = ~twos & (ones ^ num);
+            twos = ~ones & (twos ^ num);
+        }
+        return ones;
+    }
+}
--- common/question_101_to_150/single_number_ii/solution.go
+++ common/question_101_to_150/single_number_ii/solution.go
@@ -1,7 +1,7 @@
 package single_number_ii
 
 // 137. 只出现一次的数字 II https://leetcode.cn/problems/single-number-ii/
-func singleNumber3(nums []int) int {
+func singleNumber(nums []int) int {
 	once, twice := 0, 0
 	for _, v := range nums {
 		once = ^twice & (once ^ v)
