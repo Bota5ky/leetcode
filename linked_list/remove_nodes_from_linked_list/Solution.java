06384a5c9198ae9951c1606a1793a9967a84a50b
Bota5ky <54799656+Bota5ky@users.noreply.github.com>
2024-02-06 15:48:21 +0800

    pass problem 2487 - remove nodes from linked list

---
+++ linked_list/remove_nodes_from_linked_list/Solution.java
@@ -0,0 +1,46 @@
+import _model.ListNode;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/remove-nodes-from-linked-list/">2487. 从链表中移除节点</a>
+ * @since 2024-02-04 17:53
+ */
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        ListNode reversedNode = reverseNodes(head);
+        int max = reversedNode.val;
+        ListNode first = reversedNode;
+        while (reversedNode.next != null) {
+            ListNode nextNode = reversedNode.next;
+            if (nextNode.val < max) {
+                reversedNode.next = reversedNode.next.next;
+            } else {
+                max = nextNode.val;
+                reversedNode = reversedNode.next;
+            }
+        }
+        return reverseNodes(first);
+    }
+
+    private ListNode reverseNodes(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
