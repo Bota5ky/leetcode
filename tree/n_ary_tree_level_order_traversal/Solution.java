90fb6a55c48ff4cbc8aa7882c7a633c83bf91c98
Bota5ky <zjy2516@126.com>
2024-02-17 19:22:44 +0800

    pass problem 429 - n ary tree level order traversal

---
+++ tree/n_ary_tree_level_order_traversal/Solution.java
@@ -0,0 +1,57 @@
+package tree.n_ary_tree_level_order_traversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Bota5ky
+ * @link <a href="https://leetcode.cn/problems/n-ary-tree-level-order-traversal/">429. N 叉树的层序遍历</a>
+ * @since 2024-02-17 19:12
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int cnt = 1;
+        while (!queue.isEmpty()) {
+            List<Integer> layer = new ArrayList<>();
+            while (cnt > 0) {
+                cnt--;
+                Node node = queue.poll();
+                layer.add(node.val);
+                if (node.children != null) {
+                    for (Node child : node.children) {
+                        if (child != null) {
+                            queue.add(child);
+                        }
+                    }
+                }
+            }
+            cnt = queue.size();
+            res.add(layer);
+        }
+        return res;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
